cced933bc4f039947efb726b3f92ac171751eefe
hanlu <317096829@qq.com>
2019-02-12 14:50:20 +0800

    Update setting.py

--- app/setting.py
+++ app/setting.py
@@ -9,7 +9,7 @@ DEBUG = True
 
 SECRET_KEY = 'd1bd72446fbaafd5dd75778d1ba1b0e3'
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/newsoc?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/soc?charset=utf8'
 
 
 #爬虫配置文件路径
